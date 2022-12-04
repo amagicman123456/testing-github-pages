a20dd9c9527e41ca1245b074e69bdcd9800446c1
amagicman123456 <115430647+amagicman123456@users.noreply.github.com>
2022-12-04 13:38:59 -0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,38 @@
+'use strict';
+var button = document.querySelector("button")
+var Router = {
+    root: '/',
+    routes: [],
+    urls: [],
+    titles: [],
+    navigate: function() {
+        location.hash = this.root;
+        return this;
+    },
+    add: function(thePath, theUrl, theTitle) {
+        this.routes.push(thePath);
+        this.urls.push(theUrl);
+        this.titles.push(theTitle);
+    },
+
+    loader: function() {
+        this.navigate();
+        var r = this.routes;
+        var u = this.urls;
+        window.onload = function() {
+            $("#view").load("tpl/home.html");
+        };
+        window.onhashchange = function() {
+            for (var i = 0; i < r.length; i++) {
+                if (location.hash == r[i]) {
+                    $("#view").load(u[i]);
+                }
+            }
+        };
+    }
+};
+button.addEventListener('click', function(){
+    Router.add("/yes", window.location.href, "yeee");
+    Router.add("/no", window.location.href, "noo");
+    Router.loader();
+})
