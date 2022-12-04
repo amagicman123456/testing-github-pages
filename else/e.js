f50a8014c546c13026e476ba9357f88241ba72a5
amagicman123456 <115430647+amagicman123456@users.noreply.github.com>
2022-12-04 14:41:05 -0800

    Add files via upload

--- else/e.js
+++ else/e.js
@@ -1,14 +1,30 @@
 var _location = document.location.toString();
 var serverNameIndex = _location.indexOf('/', _location.indexOf('://') + 3);
 var serverName = _location.substring(0, serverNameIndex) + '/';
-var button = document.querySelector('button')
-function loading() {
-    template = [_location.hash, routes];
-    if(template == [-1]){
-        location.hash = root;
-        $("#view").load(urls[0]);
+var button = document.querySelector('button');
+var Router = {
+    root: '#/',
+    routes: [],
+    urls: [],
+    titles: [],
+    navigate: function() {
+        location.hash = this.root;
+        return this;
+    },
+    add: function(thePath, theUrl, theTitle) {
+        this.routes.push(thePath);
+        this.urls.push(theUrl);
+        this.titles.push(theTitle);
+    },
+
+    function loading() {
+        template = [_location.hash, routes];
+        if(template == [-1]){
+            _location.hash = root;
+            $("#view").load(urls[0]);
+        }
     }
-}
+};
 button.addEventListener('click', function(){
     loading('#about')
 })
